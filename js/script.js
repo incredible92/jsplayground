e9aaf373b5148f463f426a8f8ec39822d28e025a
incredible92 <sulymanabdullahi@gmail.com>
2021-06-09 17:47:00 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 const body = document.querySelector("body");
 const button = document.querySelector("playbutton");
 
-const colorArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const colorArray = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  99,
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+];
 button.addEventListener("click", switchColor);
 
 function switchColor() {}
